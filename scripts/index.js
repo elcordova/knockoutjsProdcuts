5ff8e1ebe0ca531358755a9f490ca00c7ec72a47
elcordova <erick.lenin.03@gmail.com>
2018-01-17 17:17:44 -0500

    nuevo

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+// Class to represent a row in the seat reservations grid
+function ProductoSucursal(name, producto) {
+    var self = this;
+    self.name = name;
+    self.producto = ko.observable(producto);
+    
+    self.formattedPrice=ko.computed(function(){
+        let price=self.producto().price;
+        return price ? "$"+price.toFixed(2):"-";
+    })
+}
+
+// Overall viewmodel for this screen, along with initial state
+function ProductoSucursalViewModel() {
+    var self = this;
+
+    // Non-editable catalog data - would come from the server
+    self.availableProducto = [
+        { nombre: "Fertilizante 1", price: 0 },
+        { nombre: "Fertilizantes Varios", price: 34.95 },
+        { nombre: "otros", price: 290 }
+    ];    
+
+    // Editable data
+    self.seats = ko.observableArray([
+        new ProductoSucursal("Banaoro", self.availableProducto [1]),
+        new ProductoSucursal("Agrofertil", self.availableProducto [0])
+    ]);
+    
+    self.addProducto=function(){
+        self.seats.push(new ProductoSucursal("", self.availableProducto [1]))
+    }
+    
+    self.quitarProducto=function(pro){
+        self.seats.remove(pro)
+    }
+}
+
+ko.applyBindings(new ProductoSucursalViewModel());
