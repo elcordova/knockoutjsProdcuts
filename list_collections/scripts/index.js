4a4930bee50077bdaf596f233fbefa331b899aa6
elcordova <erick.lenin.03@gmail.com>
2018-01-19 14:55:48 -0500

    manejo de lista de items y calculo de precio subtotal

--- list_collections/scripts/index.js
+++ list_collections/scripts/index.js
@@ -1,10 +1,13 @@
 // Class to represent a row in the seat reservations grid
-function ProductoSucursal(name, producto) {
+function ProductoSucursal(name, producto,cantidad=1) {
     var self = this;
     self.name = name;
     self.producto = ko.observable(producto);
+    self.cantidad=ko.observable(cantidad*1);
+    self.formattedSubtotal=ko.computed(function(){
+        return self.cantidad()*1*self.producto().price*1
+    });
     
-
     self.formattedPrice=ko.computed(function(){
         let price=self.producto().price;
         return price ? "$"+price.toFixed(2):"-";
@@ -31,13 +34,15 @@ function ProductoSucursalViewModel() {
     self.addProducto=function(){
         self.seats.push(new ProductoSucursal("", self.availableProducto [1]))
     }
+
+    self.limit=5;
     
     self.quitarProducto=function(pro){
         self.seats.remove(pro)
     }
 
     self.calcularTotales=ko.computed(function(){
-        return self.seats().reduce((ant,act)=>ant+act.producto().price,0)
+        return self.seats().reduce((ant,act)=>ant+act.formattedSubtotal()*1,0)
     });
 }
 
